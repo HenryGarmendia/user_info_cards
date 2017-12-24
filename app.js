744e298a3e6326ef0be80181f1a156786b2685f2
HenryGarmendia <me@henrygarmendia.com>
2017-12-24 00:01:41 -0600

    added an array obj to app.js and updated the index.html

---
+++ app.js
@@ -0,0 +1,18 @@
+var db = [
+    {name: "Henry Garmendia",
+    email: "henry@gmail.com",
+    age: 37,
+    bio: "Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem."},
+    {name: "Jeff Smith",
+    email: "jeff@gmail.com",
+    age: 17,
+    bio: "Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem."},
+    {name: "Anna Bananna",
+    email: "anna@gmail.com",
+    age: 27,
+    bio: "Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla quis lorem."}
+]
+
+(function avatars() {
+    
+})();
